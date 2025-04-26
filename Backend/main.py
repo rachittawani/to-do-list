9fb96b8b86884527d93173dd6f057bec73a002eb
Rachit Tawani <rachittawani418@gmail.com>
2025-04-27 01:32:40 +0530

    Fixed issue in main.py

--- Backend/main.py
+++ Backend/main.py
@@ -1,8 +1,9 @@
+import uvicorn
 from fastapi import FastAPI
 
 import db as models
-from db import engine
 from api.routes.api import router
+from db import engine
 
 app = FastAPI()
 
@@ -10,3 +11,5 @@ models.Base.metadata.create_all(bind=engine)
 
 app.include_router(router)
 
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
