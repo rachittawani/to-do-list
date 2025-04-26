e1cba721325ac257d162fb843490feebe676a923
Rachit Tawani <rachittawani418@gmail.com>
2025-04-27 02:12:50 +0530

    Changed env

--- Backend/main.py
+++ Backend/main.py
@@ -1,9 +1,15 @@
 import uvicorn
 from fastapi import FastAPI
+from dotenv import load_dotenv
+import os
 
 import db as models
 from api.routes.api import router
 from db import engine
+
+load_dotenv()
+
+PORT = int(os.getenv('PORT'))
 
 app = FastAPI()
 
@@ -12,4 +18,4 @@ models.Base.metadata.create_all(bind=engine)
 app.include_router(router)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=PORT, reload=True)
