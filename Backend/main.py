26d3a362a83cff9ed9f6653b99acb5eeb50fe7f0
Rachit Tawani <rachittawani418@gmail.com>
2025-04-27 02:23:36 +0530

    Added CORS

--- Backend/main.py
+++ Backend/main.py
@@ -1,7 +1,9 @@
-import uvicorn
-from fastapi import FastAPI
-from dotenv import load_dotenv
 import os
+
+import uvicorn
+from dotenv import load_dotenv
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 import db as models
 from api.routes.api import router
@@ -12,6 +14,14 @@ load_dotenv()
 PORT = int(os.getenv('PORT'))
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://to-do-list-inky-theta.vercel.app"],  # OR specify your frontend URL like ["https://your-frontend.com"]
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 models.Base.metadata.create_all(bind=engine)
 
